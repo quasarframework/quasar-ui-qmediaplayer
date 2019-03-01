c93df9622b8cfb5b7b09ee6b1e5997308e55ed52
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-03-01 08:24:10 -0700

    feat(boot): remove boot file when uninstalled

--- src/uninstall.js
+++ src/uninstall.js
@@ -3,7 +3,14 @@
  *
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/UninstallAPI.js
  */
+const fs = require('fs')
+const path = require('path')
 
 module.exports = function (api) {
   // TODO: remove boot file
+  let qmediaplayerBootFile = path.resolve(process.cwd(), './src/boot/qmediaplayer.js')
+  if (fs.existsSync(qmediaplayerBootFile)) {
+    fs.unlinkSync(qmediaplayerBootFile)
+    console.log(`App Extension (qmediaplayer) Info: 'qmediaplayer boot file removed'`)
+  }
 }
