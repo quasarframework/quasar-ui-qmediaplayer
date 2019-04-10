a491e7cca5daa5496ff144215cb2d111eef5bad1
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-04-10 12:40:13 -0600

    chore(folders): shuffling around for better folder structure

---
+++ src/boot/qmediaplayer.js
@@ -0,0 +1,5 @@
+import QMediaPlayer from '@quasar/quasar-app-extension-qmediaplayer/src/component/QMediaPlayer'
+
+export default async ({ Vue }) => {
+  Vue.component('q-media-player', QMediaPlayer)
+}
