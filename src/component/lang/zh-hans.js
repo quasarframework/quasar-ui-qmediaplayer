c27eaa3f96da426e697449dc94b33600f23be48a
李冬冬 <lsongzhi@163.com>
2019-05-02 11:43:49 +0800

    Update zh-hans.js, add Chinese translations

--- src/component/lang/zh-hans.js
+++ src/component/lang/zh-hans.js
@@ -1,27 +1,28 @@
 export default {
   lang: 'zh-hans',
   mediaPlayer: {
-    oldBrowserVideo: 'To view this video please enable JavaScript and/or consider upgrading to a browser that supports HTML5 video.',
-    oldBrowserAudio: 'To listen to this audio please enable JavaScript and/or consider upgrading to a browser that supports HTML5 audio.',
-    pause: 'Pause',
-    play: 'Play',
-    settings: 'Settings',
-    toggleFullscreen: 'Toggle Fullscreen',
-    mute: 'Mute',
-    unmute: 'Unmute',
-    speed: 'Speed', // Playback rate
-    language: 'Language',
-    playbackRate: 'Playback Rate',
-    waitingVideo: 'Waiting for video',
-    waitingAudio: 'Waiting for audio',
-    ratePoint5: '.5x',
-    rateNormal: 'Normal',
-    rate1Point5: '1.5x',
-    rate2: '2x',
-    trackLanguageOff: 'Off',
-    noLoadVideo: 'Unable to load video',
-    noLoadAudio: 'Unable to load audio',
-    cannotPlayVideo: 'Cannot play video',
-    cannotPlayAudio: 'Cannot play audio'
+    oldBrowserVideo: '为观看此视频，请启用 JavaScript 或者考虑升级到支持 HTML5 视频的浏览器。',
+    oldBrowserAudio: '为欣赏此音频，请启用 JavaScript 或者考虑升级到支持 HTML5 音频的浏览器。',
+    pause: '暂停',
+    play: '播放',
+    settings: '设置',
+    toggleFullscreen: '切换全屏',
+    mute: '静音',
+    unmute: '取消静音',
+    speed: '播放速率', // Playback rate
+    language: '语言',
+    playbackRate: '播放速率',
+    waitingVideo: '等待视频中',
+    waitingAudio: '等待音频中',
+    ratePoint5: '.5倍',
+    rateNormal: '正常',
+    rate1Point5: '1.5倍',
+    rate2: '2倍',
+    trackLanguageOff: '关闭',
+    noLoadVideo: '无法加载视频',
+    noLoadAudio: '无法加载音频',
+    cannotPlayVideo: '无法播放视频',
+    cannotPlayAudio: '无法播放音频'
   }
 }
+
