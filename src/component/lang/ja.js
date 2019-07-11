dd16ecd7d65af5965cb59f19be392b3690eb9032
Luca Ban <mesqueeb@users.noreply.github.com>
2019-07-11 13:13:03 +0900

    WIP japanese
    please don't merge yet. WIP. & I'm asking some friends to review.

--- src/component/lang/ja.js
+++ src/component/lang/ja.js
@@ -3,25 +3,25 @@ export default {
   mediaPlayer: {
     oldBrowserVideo: 'To view this video please enable JavaScript and/or consider upgrading to a browser that supports HTML5 video.',
     oldBrowserAudio: 'To listen to this audio please enable JavaScript and/or consider upgrading to a browser that supports HTML5 audio.',
-    pause: 'Pause',
-    play: 'Play',
-    settings: 'Settings',
-    toggleFullscreen: 'Toggle Fullscreen',
-    mute: 'Mute',
-    unmute: 'Unmute',
-    speed: 'Speed', // Playback rate
-    language: 'Language',
-    playbackRate: 'Playback Rate',
-    waitingVideo: 'Waiting for video',
-    waitingAudio: 'Waiting for audio',
+    pause: '保留', // 'Pause'
+    play: 'プレイ', // 'Play'
+    settings: '設定', // 'Settings'
+    toggleFullscreen: 'フルスクリーン', // 'Toggle Fullscreen'
+    mute: '消音', // 'Mute'
+    unmute: '消音を解除', // 'Unmute'
+    speed: 'スピード', // 'Playback rate'
+    language: '言語', // 'Language'
+    playbackRate: 'Playback Rate', // 'Playback Rate',
+    waitingVideo: '動画待機', // 'Waiting for video'
+    waitingAudio: 'オーディオ待機', // 'Waiting for audio'
     ratePoint5: '.5x',
-    rateNormal: 'Normal',
+    rateNormal: '普通', // 'Normal'
     rate1Point5: '1.5x',
     rate2: '2x',
     trackLanguageOff: 'Off',
-    noLoadVideo: 'Unable to load video',
-    noLoadAudio: 'Unable to load audio',
-    cannotPlayVideo: 'Cannot play video',
-    cannotPlayAudio: 'Cannot play audio'
+    noLoadVideo: '動画の読み込みエラー', // 'Unable to load video',
+    noLoadAudio: 'オーディオの読み込みエラー', // 'Unable to load audio',
+    cannotPlayVideo: '動画を再生できません', // 'Cannot play video',
+    cannotPlayAudio: 'オーディオを再生できません', // 'Cannot play audio'
   }
 }
