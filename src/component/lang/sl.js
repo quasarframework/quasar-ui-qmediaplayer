d643e1a4768594c93644407d1d9d3e5021e0e21e
Borut Jures <bj@properius.com>
2019-07-11 18:31:41 +0200

    Update sl.js

--- src/component/lang/sl.js
+++ src/component/lang/sl.js
@@ -1,27 +1,27 @@
 export default {
   lang: 'sl',
   mediaPlayer: {
-    oldBrowserVideo: 'To view this video please enable JavaScript and/or consider upgrading to a browser that supports HTML5 video.',
-    oldBrowserAudio: 'To listen to this audio please enable JavaScript and/or consider upgrading to a browser that supports HTML5 audio.',
-    pause: 'Pause',
-    play: 'Play',
-    settings: 'Settings',
-    toggleFullscreen: 'Toggle Fullscreen',
-    mute: 'Mute',
-    unmute: 'Unmute',
-    speed: 'Speed', // Playback rate
-    language: 'Language',
-    playbackRate: 'Playback Rate',
-    waitingVideo: 'Waiting for video',
-    waitingAudio: 'Waiting for audio',
+    oldBrowserVideo: 'Za ogled tega posnetka je potreben JavaScript. Prosim aktivirajte JavaScript in/ali razmislite o nadgradnji na brskalnik, ki podpira HTML5 video.',
+    oldBrowserAudio: 'Za poslušanje tega posnetka je potreben JavaScript. Prosim aktivirajte JavaScript in/ali razmislite o nadgradnji na brskalnik, ki podpira HTML5 avdio.',
+    pause: 'Premor',
+    play: 'Predvajaj',
+    settings: 'Nastavitve',
+    toggleFullscreen: 'Preklop na celo-stranski prikaz',
+    mute: 'Brez zvoka',
+    unmute: 'Z zvokom',
+    speed: 'Hitrost', // Playback rate
+    language: 'Jezik',
+    playbackRate: 'Hitrost predvajanja',
+    waitingVideo: 'Čakam na video posnetek',
+    waitingAudio: 'Čakam na zvočni posnetek',
     ratePoint5: '.5x',
-    rateNormal: 'Normal',
+    rateNormal: 'Normalno',
     rate1Point5: '1.5x',
     rate2: '2x',
-    trackLanguageOff: 'Off',
-    noLoadVideo: 'Unable to load video',
-    noLoadAudio: 'Unable to load audio',
-    cannotPlayVideo: 'Cannot play video',
-    cannotPlayAudio: 'Cannot play audio'
+    trackLanguageOff: 'Izklopi',
+    noLoadVideo: 'Ne morem naložiti video posnetka',
+    noLoadAudio: 'Ne morem naložiti zvočnega posnetka',
+    cannotPlayVideo: 'Ne morem predvajati video posnetka',
+    cannotPlayAudio: 'Ne morem predvajati zvočnega posnetka'
   }
 }
