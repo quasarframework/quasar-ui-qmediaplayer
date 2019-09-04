f05ae2ced2d421cc9728359007f4c0f264c478cb
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-09-04 09:35:50 -0600

    feat: new icon sets (material-design outlined, round and sharp)

---
+++ src/component/iconSet/material-icons-round.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'material-icons-round',
+  mediaPlayer: {
+    play: 'r_play_arrow',
+    pause: 'r_pause',
+    volumeOff: 'r_volume_off',
+    volumeDown: 'r_volume_down',
+    volumeUp: 'r_volume_up',
+    settings: 'r_settings',
+    speed: 'r_directions_run',
+    language: 'r_closed_caption',
+    selected: 'r_check',
+    fullscreen: 'r_fullscreen',
+    fullscreenExit: 'r_fullscreen_exit',
+    bigPlayButton: 'r_play_arrow'
+  }
+}
