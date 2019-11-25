421aedc1fd572972d7b688312ea4b09ebab12f32
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-11-25 13:42:10 -0700

    chore(docs): minor dark fixes for demo

--- demo/src/pages/Examples.vue
+++ demo/src/pages/Examples.vue
@@ -102,7 +102,11 @@ Not all languages have been translated. If you can help out, please [PR a langua
       <example-card title="Video - Icon Set" name="VideoIconSet" :tag-parts="getTagParts(require('!!raw-loader!../examples/VideoIconSet.vue').default)" />
     </div>
     <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
-      <q-btn fab icon="keyboard_arrow_up" color="primary" />
+      <q-btn
+        fab
+        icon="keyboard_arrow_up"
+        :class="{ 'text-black bg-grey-4': $q.dark.isActive, 'text-white bg-primary': !$q.dark.isActive }"
+      />
     </q-page-scroller>
   </hero>
 </template>
