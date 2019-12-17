e454bb8f678c05616cf84c35cbc5bf2c0241665a
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-17 12:41:02 -0700

    chore(ae): update compatibleWith

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -24,8 +24,8 @@ module.exports = function (api) {
   // Quasar compatibility check; you may need
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
-  api.compatibleWith('quasar', '^1.1.1')
-  api.compatibleWith('@quasar/app', '^1.1.0')
+  api.compatibleWith('quasar', '^1.5.0')
+  api.compatibleWith('@quasar/app', '^1.3.0')
 
   // Uncomment the line below if you provide a JSON API for your component
   api.registerDescribeApi('QMediaPlayer', '~@quasar/quasar-ui-qmediaplayer/dist/api/QMediaPlayer.json')
