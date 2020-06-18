92d5ba8c3d0f9beb2a42cdacabe7ae914919deaf
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-06-18 07:04:32 -0600

    fix: AE compatibility with q/app v2

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -26,7 +26,7 @@ module.exports = function (api) {
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app" CLI
   api.compatibleWith('quasar', '^1.5.0')
-  api.compatibleWith('@quasar/app', '^1.3.0') // needed for describe to work properly
+  api.compatibleWith('@quasar/app', '^1.3.0 || ^2.0.0') // needed for describe to work properly
 
   // Uncomment the line below if you provide a JSON API for your component
   api.registerDescribeApi('QMediaPlayer', '~@quasar/quasar-ui-qmediaplayer/dist/api/QMediaPlayer.json')
