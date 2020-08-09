a0826c6e6691c3d1499cc7ce2b0e0578639fb149
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-08-09 07:49:37 -0600

    chore: eslint fixes

--- demo/src/pages/MediaPlayer.vue
+++ demo/src/pages/MediaPlayer.vue
@@ -260,7 +260,8 @@ export default {
         this.sources.splice(0, this.sources.length, ...this.video[this.videoIndex].sources)
         this.tracks.splice(0, this.tracks.length, ...this.video[this.videoIndex].tracks)
         this.poster = this.video[this.videoIndex].poster
-      } else {
+      }
+      else {
         this.sources.splice(0, this.sources.length, ...this.audio.sources)
         this.tracks.splice(0, this.tracks.length)
         this.poster = ''
@@ -274,7 +275,8 @@ export default {
     nextVideo () {
       if (this.videoIndex === 0) {
         this.videoIndex = 1
-      } else {
+      }
+      else {
         this.videoIndex = 0
       }
       this.setSource()
