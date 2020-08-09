170bf6b753ad28cd9f426fb55bcb070992fcd1d1
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-08-09 10:37:23 -0600

    fix(demo): incorrect path to audio source

--- demo/src/examples/AudioSource.vue
+++ demo/src/examples/AudioSource.vue
@@ -2,7 +2,7 @@
   <div style="max-width: 800px; width: 100%;">
     <q-media-player
       type="audio"
-      source="https://github.com/quasarframework/quasar-ui-qmediaplayer/blob/dev/demo/src/media/Scott_Holmes_-_04_-_Upbeat_Party.mp3?raw=true"
+      source="https://raw.githubusercontent.com/quasarframework/quasar-ui-qmediaplayer/dev/demo/public/media/Scott_Holmes_-_04_-_Upbeat_Party.mp3"
     />
   </div>
 </template>
