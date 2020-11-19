746f2c8d1ed12ae309ebf195c1362592563c5940
Jeff Galbraith <jeff.galbraith@xendra.com>
2020-11-19 13:47:43 -0700

    chore(dev): update various files

--- ui/dev/src/router/index.js
+++ ui/dev/src/router/index.js
@@ -7,37 +7,39 @@ Vue.use(VueRouter)
 
 /*
  * If not building with SSR mode, you can
- * directly export the Router instantiation
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
  */
 
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
+    scrollBehavior (to, _, savedPosition) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          if (to.hash !== undefined && to.hash !== '') {
+            const el = document.getElementById(to.hash.substring(1))
+
+            if (el !== null) {
+              resolve({ x: 0, y: el.offsetTop - el.scrollHeight })
+              return
+            }
+          }
+
+          resolve(savedPosition || { x: 0, y: 0 })
+        }, 100)
+      })
+    },
     routes,
 
-    // Leave these as is and change from quasar.conf.js instead!
+    // Leave these as they are and change in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
   })
-
-  // we get each page from server first!
-  if (process.env.MODE === 'ssr' && process.env.CLIENT) {
-    console.log('!!!!')
-    console.log('On route change we deliberately load page from server -- in order to test hydration errors')
-    console.log('!!!!')
-
-    let reload = false
-    Router.beforeEach((to, _, next) => {
-      if (reload) {
-        window.location.href = to.fullPath
-        return
-      }
-      reload = true
-      next()
-    })
-  }
 
   return Router
 }
