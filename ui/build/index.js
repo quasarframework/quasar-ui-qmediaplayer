2d0f79a706985977ff20159d5fc46740e43de6f3
Jeff Galbraith <galbraith64@gmail.com>
2021-10-02 15:08:48 -0600

    chore: eslint fixes

--- ui/build/index.js
+++ ui/build/index.js
@@ -11,7 +11,7 @@ console.log()
 require('./script.app-ext.js').syncAppExt()
 require('./script.clean.js')
 
-console.log(` 📦 Building ${green('v' + require('../package.json').version)}...${parallel ? blue(' [multi-threaded]') : ''}\n`)
+console.log(` 📦 Building ${ green('v' + require('../package.json').version) }...${ parallel ? blue(' [multi-threaded]') : '' }\n`)
 
 createFolder('dist')
 
