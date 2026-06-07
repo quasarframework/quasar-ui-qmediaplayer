019238f462603dbb0110edd0677cb55e3913bfcd
Jeff <galbraith64@gmail.com>
2026-06-07 07:51:20 -0600

    chore: update app-vite beta.40

--- packages/app-extension/src/index.ts
+++ packages/app-extension/src/index.ts
@@ -9,7 +9,7 @@ import { defineIndexScript } from "@quasar/app-vite";
 
 export default defineIndexScript((api) => {
   api.compatibleWith("quasar", "^2.0.0");
-  api.compatibleWith("@quasar/app-vite", ">=3.0.0-beta.39");
+  api.compatibleWith("@quasar/app-vite", ">=3.0.0-beta.40");
 
   api.registerDescribeApi(
     "QMediaPlayer",
