2878589ea129aace4bdd2b38bdae89f78432388c
Jeff <galbraith64@gmail.com>
2026-06-11 06:09:11 -0600

    chore: update Quasar RC dependencies

--- packages/app-extension/src/index.ts
+++ packages/app-extension/src/index.ts
@@ -9,7 +9,7 @@ import { defineIndexScript } from "#q-app";
 
 export default defineIndexScript((api) => {
   api.compatibleWith("quasar", "^2.0.0");
-  api.compatibleWith("@quasar/app-vite", ">=3.0.0-beta.44");
+  api.compatibleWith("@quasar/app-vite", ">=3.0.0-rc.1");
 
   api.registerDescribeApi(
     "QMediaPlayer",
